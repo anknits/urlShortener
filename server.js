2243df05d7feb2ac05717da4f4bb8d73325ef936
ankur <ankuragarwall04@gmail.com>
2020-08-23 02:10:38 +0800

    redirect short urls to full urls

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const shorten = require("./routes/shorten");
 const app = express();
 const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 
 mongoose.connect(process.env.mongodbConnectionString, {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -13,6 +14,14 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     next();
+});
+
+app.get('/:shortUrl', async (req, res) => {
+    const dbUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (dbUrl == null) res.send(404);
+    dbUrl.clicks++;
+    dbUrl.save();
+    res.redirect(dbUrl.full);
 });
 
 app.use("/api/shorten", shorten);
